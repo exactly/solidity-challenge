9e52d03ff63f4e70dc6b0bcfdcfeff4a41df6e7f
Lior <97247251+lior-abadi@users.noreply.github.com>
2022-03-31 10:47:35 -0300

    Pool Arch Modification

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,8 +3,6 @@ require('dotenv').config();
 require('@openzeppelin/hardhat-upgrades');
 require("@nomiclabs/hardhat-etherscan");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -13,14 +11,9 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.9",
   // defaultNetwork: "hardhat",
 
   networks: {
