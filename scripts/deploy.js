eb811efa6e7a61174f2f49190ce61f1db85ce86d
Lior <97247251+lior-abadi@users.noreply.github.com>
2022-04-05 20:57:44 -0300

    deploy.js updated

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,5 @@
+const { ethers } = require("hardhat");
+
 async function main() {
   const [deployer] = await ethers.getSigners();
 
@@ -5,10 +7,42 @@ async function main() {
 
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  const PoolManagement = await ethers.getContractFactory("PoolManagement");
-  const poolManagement = await PoolManagement.deploy();
+  const DS = await ethers.getContractFactory("DataStorage");
+  const ds = await DS.deploy();
+  await ds.deployed();
 
-  console.log("Contract address:", poolManagement.address);
+  // Deploying TokenBalances Contract (tb):
+  const TB = await ethers.getContractFactory("TokenBalances");
+  const tb = await TB.deploy(ds.address);
+  await tb.deployed();
+
+  // Deploying rwETHToken Contract (rwETH):
+  const rwETH = await ethers.getContractFactory("rwETHToken");
+  const rweth = await rwETH.deploy(ds.address);
+  await rweth.deployed();
+
+  // Deploying PoolVault Contract (pv):
+  const PV = await ethers.getContractFactory("PoolVault");
+  const pv = await PV.deploy(ds.address);
+  await pv.deployed();
+
+  
+  // Deploying PoolClient Contract (pc):
+  const PC = await ethers.getContractFactory("PoolClient");
+  const pc = await PC.deploy(ds.address);
+  await pc.deployed();
+
+  // Deploying PoolBase Contract (pb):
+  const PB = await ethers.getContractFactory("PoolBase");
+  const pb = await PB.deploy(ds.address);
+
+  contracts = [ds, pb, tb, rweth, pv, pc];
+  contractNames = ["DataStorage", "PoolBase", "TokenBalances", "rwETHToken", "PoolVault", "PoolClient"];
+
+  for (let index = 0; index < contracts.length; index++) {
+    console.log(`${contractNames[index]} deployed with address: `, contracts[index].address);
+  }
+  
 }
 
 main()
